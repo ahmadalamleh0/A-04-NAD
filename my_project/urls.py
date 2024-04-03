dd023b614d8b789324f734411be7af1faba0bcb7
Ahmad alamleh <alamleh.ahmad@yahoo.com>
2024-04-03 03:45:12 -0400

    Register Post model for admin interface

--- my_project/urls.py
+++ my_project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.url),
+    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
