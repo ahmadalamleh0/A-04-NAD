e9ff24ac3041d91fb2561704dfe8ab61c84fd35c
Ahmad alamleh <alamleh.ahmad@yahoo.com>
2024-04-04 03:55:47 -0400

    .

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,7 +1,7 @@
 console.log('hello world')
 
 const helloWorldBox =document.getElementById('hello-world')
-
+const postsBox =document.getElementById('posts-box')
 
 
 $.ajax({
@@ -24,8 +24,18 @@ $.ajax({
     url: '/data/',
     success: function(response){
         console.log(response)
-       
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `
+        
+        ${el.title} - <b>${el.body}</b><br>
+
+        `
+        });
+ 
     },
+
     error: function(error){
         console.log(error)
     }
