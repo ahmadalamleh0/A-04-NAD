e7937d38bf5819b1b4ef60983358037186dae0c8
Ahmad alamleh <alamleh.ahmad@yahoo.com>
2024-04-04 02:34:11 -0400

    .

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -7,4 +8,7 @@ def post_list_and_create(request):
     qs = Post.objects.all()
     return render(request, 'posts/main.html', {'qs':qs})
 
+
+def hello_world_view(request):
+    return JsonResponse({'text': 'hello world'})
 
