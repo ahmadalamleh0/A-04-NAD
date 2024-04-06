5cd280d07cdb505f26b6d4d5071ae8d68e5d293b
Ahmad alamleh <alamleh.ahmad@yahoo.com>
2024-04-06 00:12:43 -0400

    Enhance AJAX success callback to dynamically update UI with new post

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -88,7 +88,7 @@ const getData = () =>{
                                 </div>
                                 <div class="col-2">
                                 <form class="like-unlike-forms" data-form-id="${el.id}">
-                                    <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`:`like (${el.count})`}</a>
+                                    <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike (${el.count})`:`like (${el.count})`}</button>
                                     </form>
                                 </div>
                              </div> 
@@ -136,6 +136,29 @@ postForm.addEventListener('submit', e=>{
         },
         success:function(response){
             console.log(response)
+            postsBox.insertAdjacentHTML('afterbegin', `
+                <div class="card mb-2">
+                    <div class="card-body"> 
+                        <h5 class="card-title">${response.title}</h5>
+                        <p class="card-text">${response.body}</p>
+                        </div>
+                            <div class="card-footer">
+                            <div class="row">
+                            <div class="col-2">
+                                <a href="#" class="btn btn-primary">Details</a>
+                            </div>
+                            <div class="col-2">
+                            <form class="like-unlike-forms" data-form-id="${response.id}">
+                                <button href="#" class="btn btn-primary" id="like-unlike-${response.id}">like (0)</button>
+                                </form>
+                            </div>
+                        </div> 
+                    </div>
+                </div>   
+        `)
+        likeUnlikePosts()
+        $('#addPostModel').model('hide')
+        
         },
         error: function(error)
         {
