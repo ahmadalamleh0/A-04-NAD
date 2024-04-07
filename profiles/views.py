2a68c95eaa854a3fa139ec549c1b05e421a4a905
Ahmad alamleh <alamleh.ahmad@yahoo.com>
2024-04-06 22:58:08 -0400

    defaining my profile view

--- profiles/views.py
+++ profiles/views.py
@@ -8,18 +8,18 @@ from django.http import JsonResponse
 def my_profile_view(request):
     obj = Profile.objects.get(user=request.user)
     form = ProfileForm(request.POST or None, request.FILES or None, instance=obj)
-    if request.is_ajax():
-        if form.is_valid():
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        if form.is_valid(): 
             instance = form.save()
             return JsonResponse({
-
-                'bio': instance.bio,
-                'avatar':instance.avatar.url,
+                'bio': instance.bio, 
+                'avatar': instance.avatar.url, 
                 'user': instance.user.username
             })
-        context = {
-            'obj': obj,
-            'form': form,
-        }
-        return render(request, 'profiles/main.html', context)
-
+            
+    context = {
+        'obj' : obj,
+        'form': form,
+    }
+    
+    return render(request, 'profiles/main.html', context)
