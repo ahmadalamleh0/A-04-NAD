b2213aed7d4c65e5bd4cec7b4041d5303a38e91c
Ahmad alamleh <alamleh.ahmad@yahoo.com>
2024-04-06 23:09:27 -0400

    Implemented AJAX form submission for profile updates with dynamic content refresh, including real-time avatar preview and success alerts, without full page reloads.

--- profiles/static/profiles/main.js
+++ profiles/static/profiles/main.js
@@ -1,1 +1,39 @@
-console.log('hello my profile')
+console.log('Hello my profiles')
+
+const avatarBox = document.getElementById('avatar-box')
+const alertBox = document.getElementById('alert-box')
+const profileForm = document.getElementById('profile-form')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
+const bioInput = document.getElementById('id_bio')
+const avatarInput = document.getElementById('id_avatar')
+
+profileForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const formData = new FormData()
+    formData.append('csrfmiddlewaretoken', csrf[0].value)
+    formData.append('bio', bioInput.value)
+    formData.append('avatar', avatarInput.files[0])
+
+    $.ajax({
+        type: 'POST',
+        url: '',
+        enctype: 'multipart/form-data',
+        data: formData,
+        success: function(response) {
+            console.log(response)
+            const avatarImage = document.getElementById('profile-avatar');
+            avatarImage.src = response.avatar;
+
+            handleAlerts('success', 'Your profile has been updated successfully!!');
+        },
+        error: function(error) {
+            console.log(error)
+        },
+
+        processData: false,
+        contentType: false,
+        cache: false,
+    })
+})
